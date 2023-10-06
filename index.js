a659b4db8c5e7c82c86b3b170d8135fe03b006a5
Diego H <diegoadonis2@gmail.com>
2023-10-05 22:55:10 -0300

    Second commit. Se modifica proyecto para cumplir con los desafios de la nueva evaluacion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan-body');
 const cors = require('cors');
-const { getPosts, insertPost } = require('./query');
+const { getPosts, insertPost, deletePost, likePost } = require('./query');
 const app = express();
 const PORT = 3000;
 
@@ -14,9 +14,33 @@ app.get('/posts', async (rep, res) => {
 	res.json(post);
 });
 app.post('/posts', async (req, res) => {
-	const post = req.body;
-	const result = await insertPost(post);
-	res.json(result);
+	try {
+		const post = req.body;
+		const result = await insertPost(post);
+		res.json(result);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+app.put('/posts/like/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = await likePost(id);
+		res.send(result);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+app.delete('/posts/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = await deletePost(id);
+		res.send(result);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 app.listen(PORT, console.log(`Servidor arriba en el puerto: ${PORT}`));
--- query.js
+++ query.js
@@ -9,15 +9,30 @@ const pool = new Pool({
 });
 
 const getPosts = async () => {
-	const result = await pool.query('select * from posts');
+	const result = await pool.query('SELECT * FROM posts;');
 	return result.rows;
 };
 
 const insertPost = async (post) => {
 	const values = Object.values(post);
-	const consult = 'insert into posts values (default, $1, $2, $3, default)';
-	const result = await pool.query(consult, values);
-	return result;
+	const consult = 'INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, DEFAULT);'; // <====
+	const result = await pool.query(consult, values);   // <====
+	return result; // <====
 };
 
-module.exports = { getPosts, insertPost };
+const likePost = async (id) => {
+	const result = await pool.query(
+		'UPDATE posts SET likes = likes + 1 WHERE id = $1;',  // <====
+		[id]
+	);
+	return result.rows;  // <====
+};
+
+const deletePost = async (id) => {
+	const { rows } = await pool.query('DELETE FROM posts WHERE id = $1;', [id]); // <====
+	return rows; // <====
+};
+
+//En las funciones insert, like y delete; se encuentran tres maneras de ingresar las querys
+
+module.exports = { getPosts, insertPost , deletePost, likePost};
